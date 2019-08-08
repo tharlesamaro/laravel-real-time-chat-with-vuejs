23e3953c60559af4ebbed74e1bf1aebacd891872
Tharles Amaro <tharlesamaro@gmail.com>
2019-08-07 23:40:32 -0300

    How to use vuejs with laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('message-component', require('./components/MessageComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +29,17 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    data: {
+        message: '',
+        chat: {
+            message: []
+        }
+    },
+    methods: {
+        send() {
+            if (this.message.length === 0) return
+            this.chat.message.push(this.message)
+            this.message = ''
+        }
+    }
 });
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -8,20 +8,32 @@
     <title>Chat</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        .list-group {
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
 
 <div class="container">
     <div class="row" id="app">
-        <ul class="list-group offset-md-4 col-4">
-            <li class="list-group-item active">Chat Room</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-            <input type="text" class="form-control" placeholder="Escreve sua mensagem...">
-        </ul>
 
+        <div class="offset-md-4 col-4">
+
+            <ul class="list-group">
+
+                <li class="list-group-item active">Chat Room</li>
+                <message-component v-for="value in chat.message">
+                    @{{ value }}
+                </message-component>
+
+            </ul>
+            <input type="text" class="form-control" placeholder="Escreve sua mensagem..." v-model="message"
+                   @keyup.enter="send">
+
+        </div>
 
     </div>
 </div>
